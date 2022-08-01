facab7b7be9afa94aa80a24f57dac7ec841bab1e
whatsuplinlin <kjm010704@gmail.com>
2022-08-01 20:43:45 +0700

    commit

---
+++ q5/src/components/EventCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="event-card">
+    <h4>{{ event.first_name }} {{ event.last_name }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  data() {
+    return {
+      event: {
+        id: 1,
+        first_name: 'Tiler',
+        last_name: 'Peile',
+        email: 'tpeile0@ycombinator.com',
+        gender: 'Male',
+        ip_address: '176.3.147.168',
+        Source: 'Oshnavīyeh',
+        Destination: 'Caobi',
+        travelDate: '6/16/2022',
+        airlineId: 2
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
---
+++ q5/src/views/EventListView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <EventCard />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    EventCard
+  }
+}
+</script>
